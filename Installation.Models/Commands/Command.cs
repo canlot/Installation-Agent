51bd5d6be6464aaceffa10048cb634c0f13d0351
canlot <jakobsasmuzki@gmx.de>
2022-12-28 00:32:53 +0100

    Further changing Command/Inform (Mediator) Pattern

--- Installation.Models/Commands/Command.cs
+++ Installation.Models/Commands/Command.cs
@@ -8,22 +8,9 @@ using Newtonsoft.Json.Converters;
 namespace Installation.Models
 {
     
-    public enum CommandAction
-    {
-        Get = 1,
-        Update = 2,
-        Abort = 4,
-        Add = 8,
-        Remove = 16,
-        Install = 32,
-        Reinstall = 64,
-        Uninstall = 128,
-        Run = 256,
-    }
 
-    abstract public class Command<T>
+    abstract public class Command
     {
-        abstract public CommandAction CommandAction { get; }
         public Command()
         {
             
@@ -36,7 +23,5 @@ namespace Installation.Models
             set { endpointId = value; }
         }
 
-
-        protected CommandAction commandAction;
     }
 }
