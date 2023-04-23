970b6a750cf6d8a528da09da31df34af17063c54
canlot <jakobsasmuzki@gmx.de>
2023-04-23 13:10:12 +0200

    Commit

--- Installation.Models/Executables/ApplicationExecutable.cs
+++ Installation.Models/Executables/ApplicationExecutable.cs
@@ -12,8 +12,6 @@ namespace Installation.Models
     [Executable("App")]
     public class ApplicationExecutable : Executable, IInstallable, IReinstallable, IUninstallable
     {
-        private string installFilePath;
-        private string installArguments;
 
         public List<ExecutableUnit> InstallableUnits { get; set; }
         public List<ExecutableUnit> UninstallableUnits { get; set; }
@@ -37,109 +35,6 @@ namespace Installation.Models
             else
                 successfulRollout = false;
         }
-        private void checkExecutor(Executor executor)
-        {
-            if (executor == null)
-                throw new NullReferenceException("No executor for this file type found");
-            if (!(executor is IApplicationExecutor))
-                throw new InvalidOperationException("This operation is not supported for this file type");
-        }
-        public async Task InstallAsync(CancellationToken cancellationToken)
-        {
-            Log.Information("Installing application: {name}", Name);
 
-            foreach (var unit in InstallableUnits)
-            {
-                
-                try
-                {
-                    CurrentlyExecuting = true;
-
-                    checkExecutor(executor);
-                    await (executor as IApplicationExecutor).InstallAsync();
-
-                    setExecutionStateFromExecutor(executor, SuccessfullInstallReturnCodes);
-                    Installed = getStateFromResult();
-                }
-                catch
-                {
-                    throw;
-                }
-                finally
-                {
-                    executor?.Dispose();
-                    CurrentlyExecuting = false;
-                }
-            }
-
-        }
-        
-
-        public async Task ReinstallAsync(CancellationToken cancellationToken)
-        {
-            if(string.IsNullOrWhiteSpace(ReinstallFilePath))
-                throw new ArgumentNullException(nameof(ReinstallFilePath));
-
-            Log.Information("Reinstalling application: {name}, file: {file}, dir {dir}", ReinstallFilePath, ExecutableDirectory);
-            
-
-            var executor = Executor.GetExecutor(ReinstallFilePath, ReinstallArguments, ExecutableDirectory, cancellationToken);
-            try
-            {
-                CurrentlyExecuting = true;
-
-                checkExecutor(executor);
-                await (executor as IApplicationExecutor).ReinstallAsync();
-
-                setExecutionStateFromExecutor(executor, SuccessfullReInstallReturnCodes);
-                ReInstalled = getStateFromResult();
-            }
-            catch
-            {
-                throw;
-            }
-            finally
-            {
-                executor?.Dispose();
-                CurrentlyExecuting = false;
-            }
-        }
-
-
-        public async Task UninstallAsync(CancellationToken cancellationToken)
-        {
-            Log.Information("Uninstalling application: {name}, file: {file}, dir: {dir}", Name, UninstallFilePath, ExecutableDirectory);
-
-
-            var executor = Executor.GetExecutor(UninstallFilePath, UninstallArguments, ExecutableDirectory, cancellationToken);
-            try
-            {
-                CurrentlyExecuting = true;
-
-                checkExecutor(executor);
-                await (executor as IApplicationExecutor).UninstallAsync();
-
-                setExecutionStateFromExecutor(executor, SuccessfullUnInstallReturnCodes);
-                UnInstalled = getStateFromResult();
-            }
-            catch
-            {
-                throw;
-            }
-            finally
-            {
-                executor?.Dispose();
-                CurrentlyExecuting = false;
-            }
-
-        }
-        
-
-        protected override void setStateToFalse()
-        {
-            installed = false;
-            reinstalled = false;
-            uninstalled = false;
-        }
     }
 }
--- Installation.Models/Executables/ScriptExecutable.cs
+++ Installation.Models/Executables/ScriptExecutable.cs
@@ -10,17 +10,14 @@ using Serilog;
 
 namespace Installation.Models
 {
-    [Executable("Script")]
     public class ScriptExecutable : Executable, IRunnable
     {
-        [ExecutableSetting]
         public string RunFilePath { get; set; }
         private bool runned;
         public bool Runned { get => runned; set { runned = value; setSuccessfulRolloutState(); OnPropertyChanged("Runned"); } }
 
 
-        [ExecutableSetting(Mandatory = false)]
-        public List<int> SuccessfullRunReturnCodes { get; set; }
+        public List<ExecutableUnit> RunnableUnits { get; set; }
 
         protected override void setSuccessfulRolloutState()
         {
@@ -31,44 +28,6 @@ namespace Installation.Models
 
         }
 
-        
-        public async Task RunAsync(CancellationToken cancellationToken)
-        {
-            Log.Information("Running Script {file} from {dir}", RunFilePath, ExecutableDirectory);
-
-            var executor = Executor.GetExecutor(RunFilePath, "", ExecutableDirectory, cancellationToken);
-            try
-            {
-                CurrentlyExecuting = true;
                 
-                checkExecutor(executor);
-                await (executor as IScriptExecutor).RunAsync();
-
-                setExecutionStateFromExecutor(executor, SuccessfullRunReturnCodes);
-                Runned = getStateFromResult();
-            }
-            catch
-            {
-                throw;
-            }
-            finally
-            {
-                executor?.Dispose();
-                CurrentlyExecuting = false;
-            }
-
-        }
-        private void checkExecutor(Executor executor)
-        {
-            if (executor == null)
-                throw new NullReferenceException("No executor for this file type found");
-            if (!(executor is IScriptExecutor))
-                throw new InvalidOperationException("This operation is not supported for this file type");
-        }
-
-        protected override void setStateToFalse()
-        {
-            Runned = false;
-        }
     }
 }
