89d77ff427d2f266f6101fb44e7b795c42536208
canlot <jakobsasmuzki@gmx.de>
2023-06-30 16:37:53 +0200

    replaced pages with controls

---
+++ Installation Agent Configurator/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Installation.Models;
+using System;
+using System.Collections.Generic;
+using System.Windows.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Installation_Agent_Configurator
+{
+    public class MainViewModel
+    {
+        public List<ExecutableBase> Executables { get; set; }
+
+        public MainViewModel() 
+        {
+            Executables = new List<ExecutableBase>();
+            Executables.Add(new ExecutableBase()
+            { Name = "Vlc Media Player"});
+            Executables.Add(new ExecutableBase()
+            {
+                Name = "Linq Pad"
+            });
+        }
+
+
+    }
+}
